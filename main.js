952c7b0c4565de100fa0f70435d239f6d1d8e43e
Akbar Ali <159713513+Akbar326@users.noreply.github.com>
2024-03-31 23:59:25 +0500

    second commit

---
+++ main.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const todoList = [];
+let condition = true;
+while (condition) {
+    const addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What do you want to add in ToDo List?\nWrite here:"
+        },
+        {
+            name: "addMore",
+            type: "list",
+            message: "Do you want to add more?\n",
+            choices: [
+                { name: "Yes", value: true },
+                { name: "No", value: false }
+            ]
+        }
+    ]);
+    todoList.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todoList);
+}
